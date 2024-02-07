0ff670f3525d9eec2079a5573ed8a6f6308673fe
vins414 <dankkyr@mail.ru>
2024-02-07 22:46:36 +0300

    1.2

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import telebot
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+bot = telebot.TeleBot(
+    token='6534512588:AAGFatTlsX_lAVNVAkNG-CsOapk3ObSzW4U')
 
+@bot.message_handler(commands=['start'])
+def main(massage):
+    bot.send_message(massage.chat.id, 'ПИСЬКА')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+bot.polling(none_stop=True)
